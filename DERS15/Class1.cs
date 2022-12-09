67ce036531da8f8d6e18dc1636a55667af314be6
Cavadxdd <cabiyevcavad@gmail.com>
2022-12-09 10:42:37 +0400

    Add project files.

---
+++ DERS15/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERS15
+{
+    internal class Class1
+    {
+        public int a = 8;
+        public int b { get; set; } = 9;
+        public static int num = 11;
+
+    }
+}
---
+++ DERS15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DERS15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 b=new Class1();
+            Console.WriteLine(Class1.num);
+            Console.ReadLine();
+
+        }
+    }
+}
